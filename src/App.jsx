ba1f3e1db173709a7dcb991fe9aa0c794010fcbc
MuhammadUsmanAfridii <70139706@student.uol.edu.pk>
2025-05-27 15:32:07 +0500

    kl

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Group 2</h1>
+      <h2>Members</h2>
+      <h3>Muhammad Usman</h3>
+      <h3>Zohaib Umer</h3>
+      <h3>Abdul Wasay</h3>
+    </>
+  )
+}
+
+export default App
